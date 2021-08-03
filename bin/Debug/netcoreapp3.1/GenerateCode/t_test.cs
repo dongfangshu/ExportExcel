eb6e686674ac480a503aaa6a594864a9724eb050
dong-fang-shu <13540390983@gitee.com>
2021-08-03 20:59:40 +0800

    模板代码

---
+++ bin/Debug/netcoreapp3.1/GenerateCode/t_test.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Beans
+{
+   public class Bean
+    {
+        
+        
+        
+        
+        ///<summary></summary>
+    }
+}
